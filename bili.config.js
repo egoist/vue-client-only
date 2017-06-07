682f7935fdf6884f75efe23d8163da588d85bf34
EGOIST <0x142857@gmail.com>
2017-06-08 00:46:40 +0800

    upgrade bili

--- bili.config.js
+++ bili.config.js
@@ -4,6 +4,8 @@ exports.format = format
 
 exports.moduleName = 'NoSSR'
 
+exports.banner = true
+
 if (format === 'umd') {
   exports.replace = {
     'process.env.NODE_ENV': JSON.stringify('production')
